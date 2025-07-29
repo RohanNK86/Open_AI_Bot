d421f05d3f004de5f18da090eeb995b8411b4774
Rohan <rohankaradigudd@gmail.com>
2025-07-29 17:07:42 +0530

    new

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 // Simple rule-based chatbot logic
 function getBotReply(message) {
   const msg = message.toLowerCase();
-  if (msg.includes('hello') || msg.includes('hi') || msg.includes('hey')) {
+  if (msg.includes('hello') || msg.includes('hi') || msg.includes('hey') || msg.includes('yoo')) {
     return 'Hello! How can I help you today?';
   } 
   else if (msg.includes('how are you')) {
@@ -27,11 +27,20 @@ function getBotReply(message) {
     return "I was built by a genius student from MS Ramaiah Institute of Technology, His Name is Rohan";
   } 
   else if (msg.includes('what all you can do for me')){
-    return "I am still being developing by the Rohan, Need to get updates a lots of tools and technology";
+    return "Tell Rohan to feed and add more features to me, so that i can do anything for you!";
   } 
   else if (msg.includes('bye')) {
-    return 'Goodbye! Have a nice day!';
+    return '🙋‍♂️Goodbye! Have a nice day!';
   } 
+  else if (msg.includes('i am fine')) {
+    return "😊That's great to hear!";
+  } 
+  else if (msg.includes('why you were created') || msg.includes('why you were developed')) {
+    return "😀Soon You'll get to know when i will bring a revolutionary changes!";
+  }
+   else if (msg.includes("who is rohan's gf")) {
+    return "🤫 Alright i will tell about it, She is very cute and beautiful and rohan truly love's her but she doest'nt know about this, I wish she will accept his proposal and marry him, cause he will take care her as a little princes";
+  }
   else {
     return `You said: "${message}"`;
   }
